a9926a5b3321d4e686fa5fb30d12f7f6d42dab8e
spacey <shaffan1996@gmail.com>
2024-11-11 16:59:49 -0500

    jobs are printing with article and comment urls, and a title :)

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,30 +1,14 @@
 use std::error::Error;
-use rss::Channel;
+use rss::{Channel, Item};
 use scraper::{Html, Selector};
 
 #[tokio::main]
 async fn main() {
     let url = "https://hnrss.org/jobs";
     let channel = get_feed(url).await.unwrap();
-    // print!("{:?}", channel.items.iter().map(f))
-    let title = channel.items[0].title.clone().unwrap();
-    let raw_description = channel.items[0].description.clone().unwrap();
-    let html = Html::parse_fragment(&raw_description);
+    let jobs = parse_items(channel.items());
 
-    let selector = Selector::parse("a").unwrap();
-    for element in html.select(&selector) {
-        if let Some(href) = element.value().attr("href") {
-            println!("Found link: {}", href);
-        }
-    }
-    println!("{:?}", title);
-    
-    /*
-    Example output:
-    "Glass Health (YC W23) is hiring founding, senior and lead full-stack engineers"
-    "<p>Article URL: <a href=\"https://jobs.lever.co/glass-health-inc?team=Product%20%26%20Engineering\">https://jobs.lever.co/glass-health-inc?team=Product%20%26%20Engineering</a></p>\n
-    <p>Comments URL: <a href=\"https://news.ycombinator.com/item?id=41976698\">https://news.ycombinator.com/item?id=41976698</a></p>\n<p>Points: 0</p>\n<p># Comments: 0</p>"
-     */
+    dbg!(jobs);
 }
 
 async fn get_feed(url: &str) -> Result<Channel, Box<dyn Error>> {
@@ -35,4 +19,38 @@ async fn get_feed(url: &str) -> Result<Channel, Box<dyn Error>> {
 
     let channel = Channel::read_from(&content[..])?;
     Ok(channel)
+}
+
+
+#[derive(Debug)]
+struct Job {
+    title: String,
+    article_url: String,
+    comments_url: Result<String, String>
+}
+
+fn parse_items(items: &[Item]) -> Vec<Job> {
+    let mut jobs = Vec::new();
+    items.iter().for_each(|item| {
+        let raw_description = item.description().unwrap();
+        let html = Html::parse_fragment(&raw_description);
+        let selector: Selector = Selector::parse("a").unwrap();
+
+        let urls = html
+            .select(&selector)
+            .filter_map(|el| el.value().attr("href"))
+            .collect::<Vec<_>>();
+
+        let title = item.title().unwrap().to_string();
+        let article_url = urls[0].to_string();
+        let comments_url = urls.get(1)
+            .ok_or("no comments url");
+        
+        jobs.push(Job {
+            title,
+            article_url,
+            comments_url
+        });
+    });
+    jobs
 }
