cbd59da026ee9c63632ac1bc949996fe45310830
spacey <shaffan1996@gmail.com>
2024-11-11 17:11:39 -0500

    changed comments url tyoe to result, in case we don't have one :)

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,6 +1,6 @@
-use std::error::Error;
 use rss::{Channel, Item};
 use scraper::{Html, Selector};
+use std::error::Error;
 
 #[tokio::main]
 async fn main() {
@@ -12,45 +12,44 @@ async fn main() {
 }
 
 async fn get_feed(url: &str) -> Result<Channel, Box<dyn Error>> {
-    let content = reqwest::get(url)
-        .await?
-        .bytes()
-        .await?;
+    let content = reqwest::get(url).await?.bytes().await?;
 
     let channel = Channel::read_from(&content[..])?;
     Ok(channel)
 }
 
-
 #[derive(Debug)]
 struct Job {
     title: String,
     article_url: String,
-    comments_url: Result<String, String>
+    comments_url: Result<String, String>,
 }
 
 fn parse_items(items: &[Item]) -> Vec<Job> {
-    let mut jobs = Vec::new();
-    items.iter().for_each(|item| {
-        let raw_description = item.description().unwrap();
-        let html = Html::parse_fragment(&raw_description);
-        let selector: Selector = Selector::parse("a").unwrap();
+    items
+        .iter()
+        .map(|item| {
+            let raw_description = item.description().unwrap();
+            let html = Html::parse_fragment(&raw_description);
+            let selector: Selector = Selector::parse("a").unwrap();
 
-        let urls = html
-            .select(&selector)
-            .filter_map(|el| el.value().attr("href"))
-            .collect::<Vec<_>>();
+            let urls = html
+                .select(&selector)
+                .filter_map(|el| el.value().attr("href"))
+                .collect::<Vec<_>>();
 
-        let title = item.title().unwrap().to_string();
-        let article_url = urls[0].to_string();
-        let comments_url = urls.get(1)
-            .ok_or("no comments url");
-        
-        jobs.push(Job {
-            title,
-            article_url,
-            comments_url
-        });
-    });
-    jobs
+            let title = item.title().unwrap().to_string();
+            let article_url = urls[0].to_string();
+            let comments_url = urls
+                .get(1)
+                .map(|url| url.to_string())
+                .ok_or(String::from("no comments url"));
+
+            Job {
+                title,
+                article_url,
+                comments_url,
+            }
+        })
+        .collect()
 }
